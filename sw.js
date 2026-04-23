aa512cd880f7d7ae0578f5acca903feb5a7808b4
youssef <104941260+aroyoop@users.noreply.github.com>
2026-04-23 12:08:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,32 +8,14 @@ const urlsToCache = [
   "./project.js"
 ];
 
-// install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Cached files");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// activate (يمسح القديم)
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
