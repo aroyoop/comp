9492e7e537e56dece6e17e8093c827431a5a0ee1
youssef <104941260+aroyoop@users.noreply.github.com>
2026-04-23 12:05:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("site-cache").then(cache => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/project.css",
-                "/index2.css",
-                "/project.js"
-            ])
-        })
-    )
-})
+const CACHE_NAME = "site-cache-v1";
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request)
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./project.css",
+  "./index2.css",
+  "./project.js"
+];
+
+// install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Cached files");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// activate (يمسح القديم)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    )
-})
+      );
+    })
+  );
+});
+
+// fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
