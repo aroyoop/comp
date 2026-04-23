f2a3d0356e3ae9626e62ae6fb2b0e9cc8a46cf2f
youssef <104941260+aroyoop@users.noreply.github.com>
2026-04-23 12:08:05 +0200

    Update project.js

--- project.js
+++ project.js
@@ -1,6 +1,3 @@
-
-
-
 let caces = {
     "Burn":[
         [
@@ -24,74 +21,31 @@ let caces = {
         ],
         "Bleeding First Aid (إسعافات النزيف)",
         "https://www.youtube.com/embed/f-icmfh97Es"
-    ],
-
-    "Fracture":[
-        [
-            "Keep the area still <br>(ثبّت الجزء المصاب)",
-            "Do not move the bone <br>(ما تحاولش تحرك العضم)",
-            "Use a splint if possible <br>(استخدم جبيرة لو تقدر)",
-            "Apply ice to reduce swelling <br>(حط تلج لتقليل الورم)",
-            "Get medical help immediately <br>(روح لدكتور فورًا)"
-        ],
-        "Fracture First Aid (إسعافات الكسور)",
-        "https://www.youtube.com/embed/2v8vlXgGXwE"
-    ],
-
-    "Heart":[
-        [
-            "Call emergency services <br>(اتصل بالإسعاف فورًا)",
-            "Help the person stay calm <br>(خلي المصاب هادي)",
-            "Loosen tight clothes<br> (فك الملابس الضيقة)",
-            "Give aspirin if safe <br>(ادي أسبرين لو ينفع)",
-            "Start CPR if unconscious<br> (ابدأ إنعاش قلبي لو فقد الوعي)"
-        ],
-        "Heart Attack First Aid (إسعافات الأزمة القلبية)",
-        "https://www.youtube.com/embed/W2WJ7z0uK6o"
-    ],
-
-    "Sprain":[
-        [
-            "Rest the injured area<br> (ريح الجزء المصاب)",
-            "Apply ice <br> (حط تلج)",
-            "Use compression bandage <br> (لف رباط ضاغط)",
-            "Elevate the area <br> (ارفع الجزء المصاب)",
-            "Avoid movement <br> (ما تحركش الجزء)"
-        ],
-        "Sprain First Aid (إسعافات الالتواء)",
-        "https://www.youtube.com/embed/3GyZ0c5y4rA"
     ]
 }
 
 function show_aid(){
-    ol.innerHTML=``
-    p.innerHTML=``
-    for(i=0;i<caces[ui.value][0].length;i++){
-        
-        ol.innerHTML+=`<li> ${caces[ui.value][0][i]}</li> `
+    let ol = document.getElementById("ol")
+    let p = document.getElementById("p")
+    let t = document.getElementById("t")
+    let frm = document.getElementById("frm")
+    let ui = document.getElementById("ui")
+
+    ol.innerHTML = ""
+    p.innerHTML = ""
+
+    for(let i = 0; i < caces[ui.value][0].length; i++){
+        ol.innerHTML += `<li>${caces[ui.value][0][i]}</li>`
     }
-    t.innerHTML=caces[ui.value][1]
-    frm.setAttribute("src" , caces[ui.value][2])
+
+    t.innerHTML = caces[ui.value][1]
+    frm.setAttribute("src", caces[ui.value][2])
 }
 
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("site-cache").then(cache => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/project.css",
-                "/index2.css",
-                "/project.js"
-            ])
-        })
-    )
-})
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request)
-        })
-    )
-})
+// تسجيل الـ Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js")
+    .then(() => console.log("SW Registered"))
+    .catch((err) => console.log("SW Error:", err));
+}
